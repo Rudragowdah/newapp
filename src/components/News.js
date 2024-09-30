b8ce84f8466b8c42ed8f330dd66a6393550fad38
Rudra Gowda H <121507406+Rudragowdah@users.noreply.github.com>
2024-09-30 21:43:12 +0530

    API Testing

--- src/components/News.js
+++ src/components/News.js
@@ -5,9 +5,19 @@ import NewsItem from './NewsItem'
 export class news extends Component {
   render() {
     return (
-      <div>
-        This is a News Component 
-        <NewsItem />
+      <div className='container'>
+        <h2>Daily News - Top Headings</h2>
+        <div className="row">
+          <div className="col-md-4">
+            <NewsItem title="myTitle" description = "desc" />
+          </div>
+          <div className="col-md-4">
+            <NewsItem title="myTitle" description = "desc" />
+          </div>
+          <div className="col-md-4">
+            <NewsItem title="myTitle" description = "desc" />
+          </div>
+        </div>
       </div>
     )
   }
